9142116fbc2d8a54580e25b1d8ff8cbea5575cdc
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-04-28 21:06:55 +0530

    updated operators

---
+++ D-Operators/6-Ternary_Operator.js
@@ -0,0 +1,21 @@
+    
+    /*** 
+     *   Ternary Operator - similar as if else condition.
+     *     
+     *    ?  True Block    :  False Block
+     **/
+
+
+    // Example 1
+
+    let age =53;
+
+    let result = (age >= 18) ? "Adult" : "Minor";
+
+    console.log(result)
+
+
+
+
+
+
