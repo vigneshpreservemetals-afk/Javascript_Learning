6a961ee0369a1124d4d0113c143948d6fa676653
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-04-28 15:19:48 +0530

    added

---
+++ D-Operators/1-Arithmetic_Opertors.js
@@ -0,0 +1,13 @@
+
+
+
+
+const a= 20;
+const b= 25;
+
+console.log("add ====>", a + b)
+console.log("sub ====>", a - b)
+console.log("mul ====>", a * b)
+console.log("mod ====>", a % b)
+console.log("div ====>", a / b)
+console.log("power ====>", a ** b)
---
+++ D-Operators/2-Assignment_Operator.js
@@ -0,0 +1,30 @@
+
+
+let x =10;
+console.log("the value of x ====>", x)
+
+
+/* Addition Assignment*/
+
+x += 25
+console.log("Addition assignment ====>", x)
+
+
+/* Subtraction Assignment*/
+
+x -= 25
+console.log("Addition assignment ====>", x)
+
+
+/* Muliplication Assignment*/
+
+x *= 25
+console.log("Multiplication assignment ====>", x)
+
+
+/* Division Assignment*/
+
+x /= 2
+console.log("Division assignment ====>", x)
+
+
