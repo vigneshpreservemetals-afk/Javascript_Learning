156a2b32bb4de268e64116d955cf0e0f059e0c52
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-04-29 22:51:12 +0530

    added while and updated forofloops

---
+++ B-Js_Loops/c-forofloops.js
@@ -0,0 +1,20 @@
+
+// // Example 1
+
+// let numbers = [10,20,30];
+
+// for(let items of numbers){
+
+//     console.log(items)
+
+// }
+
+// Example 2
+
+let text = "Hello world"
+
+for(let items of text){
+
+    console.log(items)
+    
+}
