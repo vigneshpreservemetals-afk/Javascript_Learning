63d57f40d2ec1aa92807780297bfa32b3ace9315
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-04-30 09:54:28 +0530

    updated

---
+++ A-Function/c- function_expression.js
@@ -0,0 +1,13 @@
+
+const average = function(){
+
+    let a = 10;
+    let b = 15;
+    let c = a+b/2
+
+    return c
+}
+
+let averageresult = average()
+
+console.log("averageresult===>", averageresult)
