212e0ec532889bf783d16f822d1f45a834671cd1
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-04-30 07:51:51 +0530

    function declaration/ parameterized function

--- A-Function/a-function_declaration.js
+++ A-Function/a-function_declaration.js
@@ -11,7 +11,9 @@ let greetResult = greetMessage ();
 
 console.log(greetMessage());
 
-// // Example 2
+
+
+// Example 2
 
 function additioncalculation() {
 
@@ -25,3 +27,33 @@ function additioncalculation() {
 let additionresult = additioncalculation();
 
 console.log(additioncalculation());
+
+
+
+
+// Example 3
+
+function averagecalculation() {
+
+    let a=10;
+    let b=15;
+    let c= a+b;
+    let avg = c/2;
+
+    return avg
+
+}
+
+let averageresult = averagecalculation();
+
+console.log(averagecalculation());
+
+// Example 4
+
+function simplefunction()
+{
+    console.log("Am a Simple Function")
+
+}
+
+simplefunction()
