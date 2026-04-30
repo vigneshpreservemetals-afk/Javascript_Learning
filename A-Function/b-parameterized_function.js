890a33dfe451c98d9905a6036dd21140f5a16d84
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-04-30 08:20:05 +0530

    added parameterized function

---
+++ A-Function/b-parameterized_function.js
@@ -0,0 +1,20 @@
+
+// // Example 1
+
+// function Sumofcalculation(firstnumber,secondnumber){
+//     return firstnumber+secondnumber
+// }
+// let sumofresult = Sumofcalculation();
+// console.log("sumofresult===>",sumofresult)
+
+// Example 2
+
+function subtraction(a = 10,b = 5) {
+    return a-b;
+}
+
+let subractionresult1= subtraction(5,3);
+let subractionresult2= subtraction();
+
+console.log(subractionresult1)
+console.log(subractionresult2)
