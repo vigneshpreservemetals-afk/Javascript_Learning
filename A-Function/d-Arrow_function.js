cfbe203308995b30bb85320a2e562e4d8a1f075a
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-02 08:35:22 +0530

    updated

--- A-Function/d-Arrow_function.js
+++ A-Function/d-Arrow_function.js
@@ -1,27 +1,51 @@
 
 
-// Example 1
+// // Example 1
 
-const welcomeMessage = () => {
-    return "Welcome to Student Marklist";
+// const welcomeMessage = () => {
+//     return "Welcome to Student Marklist";
+// };
+
+// const Studentresult = (m1, m2, m3) => {
+
+//     const Total = (m1 + m2 +m3);
+//     const Average = (Total/3);
+//     const Result = (m1 >= 35 && m2 >= 35 && m3 >= 35)? "Pass": "Fail";
+
+//     return{
+
+//         Studenttotal: Total,
+//         studentaverage: Average,
+//         Studentresult: Result
+//     };
+// };
+
+// const Result = Studentresult (100,25,80);
+
+// console.log(welcomeMessage())
+// console.log(Result)
+
+
+
+const Welcomefunction = () => {
+
+    return "Welcome to Students List";
 };
 
-const Studentresult = (m1, m2, m3) => {
 
-    const Total = (m1 + m2 +m3);
-    const Average = (Total/3);
-    const Result = (m1 >= 35 && m2 >= 35 && m3 >= 35)? "Pass": "Fail";
+const studentresult = (m1, m2, m3) => {
 
-    return{
+    const total= ( m1 + m2 + m3 );
+    const Average= total/3;
+    const result= m1 >= 35 && m2 >= 35 && m3 >= 35 ? "pass":"fail";
 
-        Studenttotal: Total,
+    return {
+        Studenttotal: total,
         studentaverage: Average,
-        Studentresult: Result
+        studentresult: result
     };
 };
 
-const Result = Studentresult (100,25,80);
+const result  = studentresult(100,25,80);
 
-console.log(welcomeMessage())
-console.log(Result)
-
+console.log(result)
