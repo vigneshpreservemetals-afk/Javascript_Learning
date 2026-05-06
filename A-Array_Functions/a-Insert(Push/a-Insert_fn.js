2ce8f5d316c8c61b4d6675fd662441c3275ca108
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-06 22:35:37 +0530

    Insert function added

---
+++ A-Array_Functions/a-Insert(Push/a-Insert_fn.js
@@ -0,0 +1,24 @@
+
+
+// Example 1 - Push
+
+let fruits = ["Mango","Banana","Grape"];
+
+fruits.push("kiwi");
+fruits.push("lichi");
+fruits.push("Papaya");
+fruits.push("JackFruit");
+
+// console.log("fruits===>",fruits)
+
+
+// Example 2 - unshift
+
+let fruits = ["Mango","Banana","Grape"];
+
+fruits.unshift("kiwi");
+fruits.unshift("lichi");
+fruits.unshift("Papaya");
+fruits.unshift("JackFruit");
+
+console.log("fruits===>",fruits)
