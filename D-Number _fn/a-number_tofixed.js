533f8bb4eeb52b90234be4ba5c6df45d3fffd8b2
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-06 09:05:00 +0530

    added number files

---
+++ D-Number _fn/a-number_tofixed.js
@@ -0,0 +1,102 @@
+
+
+// Example 1 - to fixed
+
+let number = 50.4754545454544
+let fixed  = number.toFixed(3)
+
+// console.log(fixed)
+
+
+
+// Example 2 - Number to string
+
+let number1 = 50.47;
+let string  = number1.toString()
+
+//console.log(string)
+
+
+
+// Example 3 - String to Integer
+
+let number2 = "100px";
+let parseInterger  = parseInt(number2)
+
+// console.log(parseInterger)
+
+
+
+// Example 4 - String to decimal
+
+let number3 = "10.25abc";
+let parsefloat  = parseFloat(number3)
+
+// console.log(parsefloat)
+
+
+
+// Example 5 - String to number
+
+let string1 = "10.25";
+let number4  = Number(string1)
+
+// console.log(Number4)
+
+
+
+// Example 6 - Is not a Number
+
+let number5 = "abc";
+let NaN  = isNaN(number5)
+
+//console.log(NaN)
+
+
+
+let number6 = 123;
+let NaN1  = isNaN(number6)
+
+// console.log(NaN1)
+
+
+
+// Example 7 - Math.round() → nearest number
+
+let number7 = 40.456;
+let MathRound  = Math.round(number7)
+
+// console.log(MathRound)
+
+
+
+// Example 8 - Math.floor() → down
+
+let number8 = 40.456;
+let MathFloor  = Math.floor(number8)
+
+// console.log(MathFloor)
+
+
+
+// Example 9 - Math.ceil() → up
+
+let number9 = 40.456;
+let MathCeil = Math.ceil(number9)
+
+// console.log(MathCeil)
+
+
+
+// Example 10 - Math.random() → random number
+
+
+let otp = Math.random()  
+
+console.log(otp)
+
+
+
+
+
+
