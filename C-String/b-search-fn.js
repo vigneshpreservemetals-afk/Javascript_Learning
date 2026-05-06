71bbcf3b66e59724b1b434df339224d43276273a
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-06 07:45:55 +0530

    string index and includes added

---
+++ C-String/b-search-fn.js
@@ -0,0 +1,42 @@
+
+// Example 1
+
+let str = " Welcome to Javascript World"
+let findtext = "World"
+
+let AvailibilityStatus = str.includes(findtext)
+let index              = str.indexOf(findtext)
+
+console.log("Index===>", index)
+
+// Example 2
+
+let str = " Welcome to Javascript World"
+let findtext = "master"
+
+let AvailibilityStatus = str.includes(findtext)
+let index              = str.indexOf(findtext)
+
+console.log("Index===>", index)
+
+
+
+// Example 3
+
+let str = " Welcome to Javascript World"
+let findtext = "JAvascript"
+
+let AvailibilityStatus = str.includes(findtext)
+let index              = str.indexOf(findtext)
+
+console.log("Index===>", index)
+
+//Example 4 
+
+let str = "Welcome to Vignesh Baskaran Portfolio"
+let findtext = "Baskaran"
+
+let AvailibilityStatus = str.includes(findtext)
+let index              = str.indexOf(findtext)
+
+console.log("Index===>",index)
