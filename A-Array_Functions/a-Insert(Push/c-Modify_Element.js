58b3607658b3994da7ecaf53ccd777e3322292fe
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-06 22:55:58 +0530

    added

---
+++ A-Array_Functions/a-Insert(Push/c-Modify_Element.js
@@ -0,0 +1,21 @@
+
+
+// Example 1 - Splice
+
+
+let fruits = ["Mango","Banana","Grape"];
+
+fruits.splice(1,0,"Papaya","DragonFruit");
+
+// console.log("fruits===>",fruits)
+
+
+
+// Example 2 - Splice
+
+
+let newfruits = ["Mango","Banana","Grape"];
+
+newfruits.splice(2,1,"Papaya","DragonFruit");
+
+console.log("newfruits===>",newfruits)
