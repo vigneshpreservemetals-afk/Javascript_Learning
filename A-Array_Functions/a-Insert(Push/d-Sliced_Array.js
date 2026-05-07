d4278d4586480989728cd71cceed790a7a3a1d1e
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-07 21:53:18 +0530

    added new branch

--- A-Array_Functions/a-Insert(Push/d-Sliced_Array.js
+++
@@ -1,7 +0,0 @@
-
-
-let fruits = ["Mango","Banana","Grape","Kiwi","Lichi"];
-
-let NewFruitsList= fruits.slice(2,4)
-
-console.log("NewFruitsList===>",NewFruitsList)
--- E-Set/a-Set.js
+++
@@ -1,8 +0,0 @@
-
-let ArrayOfNumbers =[10,20,10,20,40,50,40,30,60];
-
-let UpdatedNumbers = new Set(ArrayOfNumbers)
-
-let UpdatedSet = [...UpdatedNumbers]
-
-console.log("UpdatedSet===>",UpdatedSet)
