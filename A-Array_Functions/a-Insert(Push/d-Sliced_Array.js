45e1107b4b641a742f82c5e850b62e8fd66bb145
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-07 07:16:03 +0530

    added splice,slice and to string, join

---
+++ A-Array_Functions/a-Insert(Push/d-Sliced_Array.js
@@ -0,0 +1,7 @@
+
+
+let fruits = ["Mango","Banana","Grape","Kiwi","Lichi"];
+
+let NewFruitsList= fruits.slice(2,4)
+
+console.log("NewFruitsList===>",NewFruitsList)
---
+++ A-Array_Functions/a-Insert(Push/e-Convert_String.js
@@ -0,0 +1,11 @@
+
+let fruits = ["Mango","Banana","Grape","Kiwi","Lichi"];
+
+
+let ArrayToString = fruits.toString();
+let ChangeSpecialCharacter =fruits.join(" * ");
+let ChangeSpecialCharacter1 =fruits.join(" # ");
+
+console.log("ArrayToString===>", ArrayToString)
+console.log("ChangeSpecialCharacter===>", ChangeSpecialCharacter)
+console.log("ChangeSpecialCharacter1===>", ChangeSpecialCharacter1)
