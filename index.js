ee80379e1ce0f7675d812ad8bb855ed5d2674604
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-13 08:20:41 +0530

    added sellecting element

---
+++ index.js
@@ -0,0 +1,12 @@
+
+const message = document.getElementById("Welcome-Message").innerText
+const colorList = document.getElementsByClassName("color")
+const tagSelector = document.getElementsByTagName("h5")
+
+const querySelector = document.querySelectorAll(".color")
+
+// console.log("message===>",message)
+console.log("colorList===>",colorList[1].innerText)
+console.log("tagSelector===>",tagSelector[0].innerText)
+console.log("querySelector===>",querySelector[1].innerText)
+
