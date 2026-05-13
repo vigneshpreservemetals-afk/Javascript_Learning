4a3c3cf5f437c39ed7fd5c2b354173ce2bb6121c
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-13 12:21:00 +0530

    added tempalte literals

---
+++ D-Notes/a-Template_Literals.js
@@ -0,0 +1,19 @@
+
+
+const employeeName          = "VigneshBaskaran"
+const employeeQualification = "BE- Mechanical"
+const employeeProfession     = "IT-SoftwareEngineer"
+
+console.log("employeeName===>",employeeName, "employeeQualification===>",employeeQualification, "empolyeeProfession===>",employeeProfession )
+
+
+console.log(`employeeName===> ${employeeName} employeeQualification===> ${employeeQualification} empolyeeProfession===> ${employeeProfession}`)
+
+
+const data = `
+Am Vignesh Baskaran
+I have 10 years of experience in FullStack Development
+`
+
+
+console.log(`data===>${data}`)
