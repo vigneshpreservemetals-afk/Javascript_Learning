60194fe42d48513dbc50601075ce2c07e99d7d70
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-13 12:56:01 +0530

    adding destructuring

---
+++ D-Notes/b-Desctructuring.js
@@ -0,0 +1,68 @@
+
+
+// A - Array Destructuring
+
+// Example 1: Basic example
+
+
+const arr = [10,20,30,40];
+
+const [a,b,c,d] = arr; 
+
+console.log(a,b,c,d)
+
+
+
+// Example 2: Skip Values
+
+
+const arr1 = [10,20,30,40];
+
+const [e, ,f,g] = arr1; 
+
+console.log(e,f,g)
+
+
+
+// Example 3: Default Values
+
+
+const [h=10, i=5] = [1,6]
+
+console.log(h,i)
+
+
+
+// Example 4: Swap Values
+
+
+let x = 20;
+let y = 25;
+
+[x,y] = [y,x];
+
+console.log(x,y)
+
+
+// Example 5: Swap Values
+
+
+// let x = 20;
+// let y = 25;
+
+// [x,y] = [y,x];
+
+// console.log(x,y)
+
+
+// B - Object Destructuring
+
+const user ={
+    name: "Vignesh",
+    age: 31
+}
+
+const {name,age} = user;
+
+console.log(name,age)
+
