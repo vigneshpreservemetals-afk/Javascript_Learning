b09a61f550231c6c852365e25c7eec9d681facba
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-14 07:35:39 +0530

    nullish Operator and optional chanining added

---
+++ D-Notes/e-Optional_Chaining.js
@@ -0,0 +1,16 @@
+
+
+const user ={
+
+    profile:{
+
+        personalInfo: {
+
+            city:"Chennai"
+
+        }
+    }
+}
+
+console.log(user.profile?.personalInfo?.city)
+console.log(user.job?.profile?.company)
---
+++ D-Notes/f-Nullish_Operator.js
@@ -0,0 +1,13 @@
+
+
+const candidateName = null;
+
+const price = 0;
+
+console.log(candidateName?? "Vignesh")
+console.log(price ?? 100)
+
+console.log("------------------------------------")
+
+console.log(candidateName || "Vignesh")
+console.log(price || 100)
