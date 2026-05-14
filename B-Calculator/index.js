2f148d93ab9d49b62041a14dccefe063b36b9f6a
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-14 08:20:24 +0530

    added new files

---
+++ B-Calculator/index.js
@@ -0,0 +1,11 @@
+
+
+import {add,sub,mul,div,square} from './index.mjs'
+
+const input1 = 100;
+
+const input2 = 200;
+
+const result = add(input1,input2)
+
+console.log(result)
---
+++ B-Calculator/index.mjs
@@ -0,0 +1,7 @@
+
+
+export const add = (input1,input2) => input1 + input2
+export const sub = (input1,input2) => input1 - input2
+export const mul = (input1,input2) => input1 * input2
+export const div = (input1,input2) => input1 / input2
+export const square = (input1,input2) => input1 ** input2
