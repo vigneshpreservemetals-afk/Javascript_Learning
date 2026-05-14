559798a71ed3eb67b839d5abf89823db11bb752a
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-14 08:23:46 +0530

    added calculator

--- B-Calculator/index.js
+++ B-Calculator/index.js
@@ -7,5 +7,13 @@ const input1 = 100;
 const input2 = 200;
 
 const result = add(input1,input2)
+const result1 = mul(input1,input2)
+const result2 = div(input1,input2)
+const result3 = sub(input1,input2)
+const result4 = square(input1,input2)
 
-console.log(result)
+console.log("add===>=", result)
+console.log("mul===>=", result1)
+console.log("div===>=", result2)
+console.log("sub===>=", result3)
+console.log("square===>=", result4)
