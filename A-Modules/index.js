89b6f6044aa97135b9bcee5f827b1d33c5503015
vigneshpreservemetals-afk <vignesh.preservemetals@gmail.com>
2026-05-14 07:46:58 +0530

    added modules

---
+++ A-Modules/index.js
@@ -0,0 +1,7 @@
+
+
+import {add} from './index.mjs'
+
+const result = add(10,15)
+
+console.log("result===>", result)
---
+++ A-Modules/index.mjs
@@ -0,0 +1,4 @@
+
+export const title = "calculator";
+
+export const add = (a,b) => (a+b);
